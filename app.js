606457196cfc4041e05bfd489e0d45093dda81eb
Alex Cahn <alexcahn@gmail.com>
2017-01-18 08:35:25 -0800

    got the images centeredand sized

--- app.js
+++ app.js
@@ -15,34 +15,34 @@ var productTwo = document.createElement('pic-2');
 var productThree = document.createElement('pic-3');
 
 //constructor function
-function ProductPic(productName, clicked, shown, imagePath){
+function ProductPic(productName, imagePath){
   this.productName = productName;
-  this.clicked = clicked;
-  this.shown = shown;
+  this.clicked = 0;
+  this.shown = 0;
   this.imagePath = imagePath;
 };
 
 //adding our constructor objects
-var bag = new ProductPic(productName[0], 0, 0, imagePath[0]);
-var banana = new ProductPic(productName[1], 0, 0, imagePath[1]);
-var bathroom = new ProductPic(productName[2], 0, 0, imagePath[2]);
-var boots = new ProductPic(productName[3], 0, 0, imagePath[3]);
-var breakfast = new ProductPic(productName[4], 0, 0, imagePath[4]);
-var bubblegum = new ProductPic(productName[5], 0, 0, imagePath[5]);
-var chair = new ProductPic(productName[6], 0, 0, imagePath[6]);
-var cthulhu = new ProductPic(productName[7], 0, 0, imagePath[7]);
-var dogDuck = new ProductPic(productName[8], 0, 0, imagePath[8]);
-var dragon = new ProductPic(productName[9], 0, 0, imagePath[9]);
-var pen = new ProductPic(productName[10], 0, 0, imagePath[10]);
-var petSweep = new ProductPic(productName[11], 0, 0, imagePath[11]);
-var scissors = new ProductPic(productName[12], 0, 0, imagePath[12]);
-var shark = new ProductPic(productName[13], 0, 0, imagePath[13]);
-var sweep = new ProductPic(productName[14], 0, 0, imagePath[14]);
-var tauntaun = new ProductPic(productName[15], 0, 0, imagePath[15]);
-var unicorn = new ProductPic(productName[16], 0, 0, imagePath[16]);
-var usb = new ProductPic(productName[17], 0, 0, imagePath[17]);
-var waterCan = new ProductPic(productName[18], 0, 0, imagePath[18]);
-var wineGlass = new ProductPic(productName[19], 0, 0, imagePath[19]);
+var bag = new ProductPic(productName[0], imagePath[0]);
+var banana = new ProductPic(productName[1], imagePath[1]);
+var bathroom = new ProductPic(productName[2], imagePath[2]);
+var boots = new ProductPic(productName[3], imagePath[3]);
+var breakfast = new ProductPic(productName[4], imagePath[4]);
+var bubblegum = new ProductPic(productName[5], imagePath[5]);
+var chair = new ProductPic(productName[6], imagePath[6]);
+var cthulhu = new ProductPic(productName[7], imagePath[7]);
+var dogDuck = new ProductPic(productName[8], imagePath[8]);
+var dragon = new ProductPic(productName[9], imagePath[9]);
+var pen = new ProductPic(productName[10], imagePath[10]);
+var petSweep = new ProductPic(productName[11], imagePath[11]);
+var scissors = new ProductPic(productName[12], imagePath[12]);
+var shark = new ProductPic(productName[13], imagePath[13]);
+var sweep = new ProductPic(productName[14], imagePath[14]);
+var tauntaun = new ProductPic(productName[15], imagePath[15]);
+var unicorn = new ProductPic(productName[16], imagePath[16]);
+var usb = new ProductPic(productName[17], imagePath[17]);
+var waterCan = new ProductPic(productName[18], imagePath[18]);
+var wineGlass = new ProductPic(productName[19], imagePath[19]);
 
 var productPicArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
 
@@ -64,15 +64,7 @@ function displayImg() {
   }
 };
 displayImg();
-
-
-
-
-
-
-
-
-
+//while statement to make sure duplicate images don't show
 
 //   var leftIndex = randNum();
 //   var leftProduct = productPicArray[leftIndex];
